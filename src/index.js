c4b1aa4f9636396bdd41828b5c85aaf928a1a8b9
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-01-12 21:51:24 +0530

    connect to DB

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('hi')
+import 'dotenv/config'
+import {connectDB} from './db/index.js';
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+/*import mongoose from 'mongoose';
+import {DB_NAME} from './constants.js';
+import express from 'express';
+const app = express();
+(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on('error',(err)=>{
+            console.error("Error: ",err);
+            throw err;
+        });
+        app.listen(process.env.PORT,()=>{
+            console.log("Server run at: http://localhost:"+process.env.PORT);
+        })
+    }catch(err){
+        console.error('Error: ',err);
+        throw err;
+    }
+})()*/
