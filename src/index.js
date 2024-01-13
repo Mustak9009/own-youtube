b073c1a76a4b8fcde9d38dac47ce01bf77df2d6f
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-01-13 09:09:53 +0530

    add app configaration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import 'dotenv/config'
 import {connectDB} from './db/index.js';
-connectDB();
+import {app} from './app.js';
+connectDB().then(()=>{
+    app.on('error',(err)=>{
+        console.error("Express error: ",err);
+        throw err;
+    });
+    app.listen(process.env.PORT,()=>{
+        console.log('Server run at http://localhost:'+process.env.PORT);
+    })
+}).catch((err)=>{
+    console.log('MongoDB Connection FAILD: ',err);
+})
 
 
 
