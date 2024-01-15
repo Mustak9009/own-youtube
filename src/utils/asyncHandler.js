6a7512fb3ccdd748c4ea711538543c3f889a8a67
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-01-15 23:06:57 +0530

    add comment

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,9 +1,10 @@
+//Style - 1
 export const asyncHandler  = (asyncFunction)=>{
     (req,res,next) =>{
         Promise.resolve(asyncFunction(req,res,next)).catch((err)=>next(err))
     }
 }
-
+//Style - 2
 export const asyncHandler2 =  (asyncFunction) => async(req,res,next)=>{
     try{
         await asyncFunction(req,res,next);
