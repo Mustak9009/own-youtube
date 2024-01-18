5b6c714af2729cba2c7b9c09e5e3a686cf4227fe
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-01-18 21:33:11 +0530

    add

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 const userSchema = new mongoose.Schema({
     userName:{
         type:String,
@@ -41,5 +43,39 @@ const userSchema = new mongoose.Schema({
     coverImage:String,
     refreshToken:String,
 },{timestamps:true});
-
+userSchema.pre('save',async function (next){ //Middleware to => 'hash' password
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+    
+});
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            id:this._id
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            id:this._id,
+            userName:this.userName,
+            fullName:this.fullName,
+            email:this.email,
+            avatar:this.avaTar
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model('User',userSchema);
