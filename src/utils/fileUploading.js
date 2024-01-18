cc0ba36ef74d8d419f1eb225395fada280a5908a
Mustak sk <91077142+Mustak9009@users.noreply.github.com>
2024-01-18 21:46:53 +0530

    add cloud uploading code

--- src/utils/fileUploading.js
+++ src/utils/fileUploading.js
@@ -6,11 +6,16 @@ cloudinary.config({
   api_key:process.env.CLOUDINARY_API_KEY  , 
   api_secret: process.env.CLOUDINARY_API_SECRET 
 });
-const uploadOnCloudinary = async (localPath)=>{
+const uploadOnCloudinary = async (locaFilePath)=>{
     try{
-        if(!localPath) return {error:'File path does not found...!!!'}
-        cloudinary.uploader.upload(localPath,{resource_type:'auto'});
+        if(!locaFilePath) return {error:'File path does not found...!!!'}
+        const res = await cloudinary.uploader.upload(locaFilePath,{resource_type:'auto'});
+        console.log("File uploading response: ",res);
+        return res;
     }catch(err){
+        fs.unlinkSync(locaFilePath);
         console.log(err);
+        return null;
     }
 }
+export {uploadOnCloudinary}
